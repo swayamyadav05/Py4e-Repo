7bdff5d6be8b9b4d8a3c01dbd3d3dbe5d484f84d
Mangloo69 <yadav69.swayam@gmail.com>
2024-03-13 01:09:04 +0530

    Chapter 14

---
+++ Chapter 14 (Object Oriented Programming)/04_class&object.py
@@ -0,0 +1,20 @@
+class PartyAnimal:
+
+    def __init__(self, z):
+        self.x = 0
+        self.name = z
+        print(self.name, "constructed")
+
+    def party(self):
+        self.x = self.x + 1
+        print(self.name, "party count", self.x)
+
+
+s = PartyAnimal("Sally")
+s.party()
+j = PartyAnimal("Jim")
+
+j.party()
+s.party()
+
+# Constructors can have additional parameters. These can be used to set up instance variables for the particular instance of the class (i.e., for the particular object)
---
+++ Chapter 14 (Object Oriented Programming)/find_capabilities.py
@@ -0,0 +1,10 @@
+# A nerdy way to find Capabilities
+
+y = list()
+print(type(y))
+print(dir(y))
+
+# The dir() command lists capabilities
+
+
+# Ignore the ones with underscores - these are used by Python itself
