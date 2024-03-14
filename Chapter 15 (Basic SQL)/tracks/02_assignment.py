56f16d8ba7a18f01a2cb2ed5f80cc8984d75d2cb
Mangloo69 <yadav69.swayam@gmail.com>
2024-03-15 00:54:54 +0530

    music track db

--- Chapter 15 (Basic SQL)/tracks/02_assignment.py
+++ Chapter 15 (Basic SQL)/tracks/02_assignment.py
@@ -105,6 +105,7 @@ sqlstr = """
 SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN Album JOIN Artist ON Track.genre_id = Genre.ID and Track.album_id = Album.id AND Album.artist_id = Artist.id ORDER BY Artist.name LIMIT 3
 """
 # """SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN Album JOIN Artist ON Track.genre_id = Genre.id AND Track.album_id = Album.id AND Album.artist_id = Artist.id ORDER BY Artist.name LIMIT 3"""
+
 for row in cur.execute(sqlstr):
     print(f"{str(row[0])} | {str(row[1])} | {str(row[2])} | {str(row[3])}")
 
