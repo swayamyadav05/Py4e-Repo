7b5c9dbf21eaa2c044fdfcbab2df76e08da0a602
Mangloo69 <yadav69.swayam@gmail.com>
2024-03-18 23:13:43 +0530

    Capstone Working

---
+++ Capstone/file.py
@@ -0,0 +1,73 @@
+# print(ord("G"))
+
+
+# x = int(input("Enter x: "))
+# if x < 2:
+#     print("Below 2")
+# elif x < 0:
+#     print("Negative")
+# else:
+#     print("Something else")
+
+# zap = "hello there bob"
+# print(zap[4])
+
+
+# def fred():
+#     print("Zap")
+
+# def jane():
+#     print("ABC")
+
+# jane()
+# fred()
+# jane()
+
+
+# stuff = ["joseph", "sally", "walter", "tim"]
+# print(stuff[2])
+
+
+# def hello():
+#     print("Hello")
+#     print("There")
+
+
+# x = 10
+# x = x + 1
+
+
+# x = -1
+# for value in [3, 41, 12, 9, 74, 15]:
+#     if value > x:
+#         x = value
+# print(x)
+
+
+# total = 0
+# for abc in range(5):
+#     total += abc
+# print(total)
+
+
+# a = "123"
+# b = 456
+# c = a + b
+# print(c)
+
+
+# print("Line1")
+# try:
+#     print("Line2")
+#     print("Line3")
+#     print("Line4")
+
+# except:
+#     print("Line5")
+# print("Line6")
+
+
+# abc = "With three words"
+# stuff = abc.split()
+# print(stuff)
+# print(len(stuff))
